2fa6929b19dad701831c56379bd6af09a1e6b175
KentBen <kentben@gmail.com>
2013-12-16 00:23:15 +0000

    commented out render stuff

--- cookies/GiftBoxRenderer.java
+++ cookies/GiftBoxRenderer.java
@@ -22,29 +22,29 @@ public class GiftBoxRenderer extends TileEntitySpecialRenderer{
 
 	public void renderAModelAt(GiftBoxEntity par1GiftBoxEntity, double par2, double par4, double par6, float par8 ){
 		
-		int metadata = par1GiftBoxEntity.getBlockMetadata();
-		int rotationAngle = 0;
+	//	int metadata = par1GiftBoxEntity.getBlockMetadata();
+	//	int rotationAngle = 0;
 		
-		if(metadata%4 == 0){
-			rotationAngle = 0;
-		}
-		if(metadata % 4 == 1){
-			rotationAngle = 270;
-		}
-		if(metadata % 4 == 2){
-			rotationAngle = 180;
-		}
-		if(metadata % 4 == 3){
-			rotationAngle = 90;
-		}
+	//	if(metadata%4 == 0){
+	//		rotationAngle = 0;
+	//	}
+	//	if(metadata % 4 == 1){
+	//		rotationAngle = 270;
+	//	}
+	//	if(metadata % 4 == 2){
+	//		rotationAngle = 180;
+	//	}
+	//	if(metadata % 4 == 3){
+	//		rotationAngle = 90;
+	//	}
 		
-		GL11.glPushMatrix();
-		GL11.glTranslated((float) par2 + 0.5F, (float)par4 + 1.5F , (float)par6 + 0.5F);
-		GL11.glScalef(1.0F, -1F, -1F);
-		GL11.glRotatef(rotationAngle * 90 , 0.0F, 1.0F, 0.0F);
-		this.bindTexture(GiftBoxLocation);
-		this.model.renderAll();
-		GL11.glPopMatrix();
+	//	GL11.glPushMatrix();
+	//	GL11.glTranslated((float) par2 + 0.5F, (float)par4 + 1.5F , (float)par6 + 0.5F);
+	//	GL11.glScalef(1.0F, -1F, -1F);
+	//	GL11.glRotatef(rotationAngle * 90 , 0.0F, 1.0F, 0.0F);
+	//	this.bindTexture(GiftBoxLocation);
+	//	this.model.renderAll();
+	//	GL11.glPopMatrix();
 		
 		
 	}
